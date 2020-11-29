aa0cf070fe84b23d776b4f11783efea080a34f3c
jeremytrindade <jeremytrindade@gmail.com>
2020-11-29 07:21:54 +0100

    Correct some sintaxe on model files

--- backoffice/modelos/usuarios.modelo.php
+++ backoffice/modelos/usuarios.modelo.php
@@ -10,25 +10,28 @@ Class ModeloUsuarios{
 
 	static public function mdlRegistroUsuario($tabla, $datos){
 
-		$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(perfil, nombre, email, password, suscripcion, verificacion, email_encriptado, patrocinador) VALUES (:perfil, :nombre, :email, :password, :suscripcion, :verificacion, :email_encriptado, :patrocinador)"); //termino muito ussado para conexao de db
+		$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(perfil, nombre, email, password, suscripcion, verificacion, email_encriptado, patrocinador) VALUES (:perfil, :nombre, :email, :password, :suscripcion, :verificacion, :email_encriptado, :patrocinador)");//termino muito ussado para conexao de db
 
-		$stmt->binParam(":perfil", $datos["perfil"], PDO::PARAM_STR);
-		$stmt->binParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
-		$stmt->binParam(":email", $datos["email"], PDO::PARAM_STR);
-		$stmt->binParam(":password", $datos["password"], PDO::PARAM_STR);
-		$stmt->binParam(":suscripcion", $datos["suscripcion"], PDO::PARAM_STR);
-		$stmt->binParam(":verificacion", $datos["verificacion"], PDO::PARAM_STR);
-		$stmt->binParam(":email_encriptado", $datos["email_encriptado"], PDO::PARAM_STR);
-		$stmt->binParam(":patrocinador", $datos["patrocinador"], PDO::PARAM_STR);//lançar parametro
+		$stmt->bindParam(":perfil", $datos["perfil"], PDO::PARAM_STR);
+		$stmt->bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
+		$stmt->bindParam(":email", $datos["email"], PDO::PARAM_STR);
+		$stmt->bindParam(":password", $datos["password"], PDO::PARAM_STR);
+		$stmt->bindParam(":suscripcion", $datos["perfil"], PDO::PARAM_STR);
+		$stmt->bindParam(":verificacion", $datos["verificacion"], PDO::PARAM_STR);
+		$stmt->bindParam(":email_encriptado", $datos["email_encriptado"], PDO::PARAM_STR);
+		$stmt->bindParam(":patrocinador", $datos["patrocinador"], PDO::PARAM_STR);//lançar parametro
 
 		if($stmt->execute()){
 
 			return "ok";
+
 		}else{
-			return print_r(Connexion::conectar()->errorInfo());
+
+			return print_r(Conexion::conectar()->errorInfo());
 		}
 
 		$stmt->close();//por segurança
 		$stmt = null;//por segurança
-	}	
+
+	}
 }
